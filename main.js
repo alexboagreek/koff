d0553cc59bcd464a4c24840046a3fc58b7f6928c
Alex <olivesargreek@gmail.com>
2023-10-06 18:56:00 +0300

    start: OOP is PAIN

--- main.js
+++ main.js
@@ -1,28 +1,61 @@
-import 'normalize.css';
-import './style.scss';
+import "normalize.css";
+import "./style.scss";
 
-import Swiper from 'swiper';
-import { Navigation, Thumbs } from 'swiper/modules';
-import 'swiper/css';
+import { Header } from "./modules/Header/Header";
 
-const swiperThumbnails = new Swiper(".product__slider-thumbnails", {
-  loop: true,
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-new Swiper(".product__slider-main", {
-  loop: true,
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".product__arrow-next",
-    prevEl: ".product__arrow-prev",
-  },
-  modules: [Navigation, Thumbs],
-  thumbs: {
-    swiper: swiperThumbnails,
-  },
-});
+import Navigo from "navigo";
 
+const productSlider = () => {
+  Promise.all([
+    import("swiper/modules"),
+    import("swiper"),
+    import("swiper/css"),
+  ]).then(([{ Navigation, Thumbs }, Swiper]) => {
+    const swiperThumbnails = new Swiper.default(".product__slider-thumbnails", {
+      loop: true,
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
+    new Swiper.default(".product__slider-main", {
+      loop: true,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".product__arrow-next",
+        prevEl: ".product__arrow-prev",
+      },
+      modules: [Navigation, Thumbs],
+      thumbs: {
+        swiper: swiperThumbnails,
+      },
+    });
+  });
+};
 
+const init = () => {
+
+  new Header().mount();
+  
+  productSlider();
+
+  const router = new Navigo("/", { linksSelector: 'a[href^="/"]' });
+
+  router
+    .on("/", () => {
+      console.log("Main...");
+    })
+    .on("/category", () => {console.log('category');})
+    .on("/favorite", () => {console.log('favorite');})
+    .on("/search", () => {console.log('search');})
+    .on("/product/:id", () => {})
+    .on("/cart", () => {})
+    .on("/order", () => {})
+    .on("/category", () => {})
+    .notFound(() => {
+      console.log(404);
+    });
+  router.resolve();
+};
+
+init();
